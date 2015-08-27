692e0fbc758e128d0dde1662c10a77658c3863c8
Bogdan Chadkin <trysound@yandex.ru>
2015-08-27 22:44:44 +0300

    fixed build

--- dist/transition-to-from-auto.js
+++ dist/transition-to-from-auto.js
@@ -1,121 +1,151 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.transitionToFromAuto = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-"use strict";
+/*!
+ * transition-to-from-auto 0.5.2
+ * https://github.com/75lb/transition-to-from-auto
+ * Copyright 2015 Lloyd Brookes <75pound@gmail.com>
+ */
 
 /**
-@module transition-to-from-auto
+@module
+@alias transition
 */
-module.exports = transition;
+(function(window, document){
+    "use strict";
 
-var isTransition = "data-ttfaInTransition";
-var elements = [];
-var data = [];
+    var getComputedStyle = window.getComputedStyle;
+    var isTransition = "data-ttfaInTransition";
 
-// Transition detecting
-var transitionProp = false;
-var transitionEnd = false;
-var testStyle = document.createElement("a").style;
-var testProp;
+    var elements = [];
+    var data = [];
 
-if(testStyle[testProp = "webkitTransition"] !== undefined) {
-    transitionProp = testProp;
-    transitionEnd = testProp + "End";
-}
+    // Transition detecting
+    var transitionProp = false;
+    var transitionEnd = false;
+    var testStyle = document.createElement("a").style;
+    var testProp;
 
-if(testStyle[testProp = "transition"] !== undefined) {
-    transitionProp = testProp;
-    transitionEnd = testProp + "end";
-}
-
-function process(options, data) {
-    var el = options.element;
-    var val = options.val;
-    var prop = options.prop;
-    var style = el.style;
-    var startVal;
-    var autoVal;
-
-    if(!transitionProp) {
-        return style[prop] = val;
+    if(testStyle[testProp = "webkitTransition"] !== undefined) {
+        transitionProp = testProp;
+        transitionEnd = testProp + "End";
     }
 
-    if(el.hasAttribute(isTransition)) {
-        el.removeEventListener(transitionEnd, data.l);
-    } else {
-        style[transitionProp] = "none";
+    if(testStyle[testProp = "transition"] !== undefined) {
+        transitionProp = testProp;
+        transitionEnd = testProp + "end";
+    }
 
-        startVal = getComputedStyle(el)[prop];
-        style[prop] = "auto";
-        autoVal = getComputedStyle(el)[prop];
+    function process(options, data) {
+        var el = options.element;
+        var val = options.val;
+        var prop = options.prop;
+        var style = el.style;
+        var startVal;
+        var autoVal;
 
-        // Interrupt
-        if(startVal === val || val === "auto" && startVal === autoVal) {
-            return;
+        if(!transitionProp) {
+            return style[prop] = val;
         }
 
-        data.auto = autoVal;
-        el.setAttribute(isTransition, 1);
-
-        // Transition
-        style[prop] = startVal;
-        el.offsetWidth;
-        style[transitionProp] = options.style;
-    }
-
-    style[prop] = val === "auto" ? data.auto : val;
-
-    data.l = function (e) {
-        if(e.propertyName === prop) {
-            el.removeAttribute(isTransition);
+        if(el.hasAttribute(isTransition)) {
             el.removeEventListener(transitionEnd, data.l);
-            if(val === "auto") {
-                /* avoid transition flashes in Safari */
-                style[transitionProp] = "none";
-                style[prop] = val;
-            }
-        }
-    };
-
-    el.addEventListener(transitionEnd, data.l);
-}
-
-/**
-transition to and from auto
-@param options {Object} - the options
-@param options.element {mixed} - the element or selector
-@param options.val {string} - the transition destination
-@param [options.prop] {string} - the CSS property to transition, defaults to `height`
-@param [options.style] {string} - the CSS transition style, defaults to css
-@alias module:transition-to-from-auto
-*/
-function transition(options){
-    var element = options.element;
-    var datum;
-    var index;
-
-    if(typeof element === "string") {
-        element = document.querySelector(element);
-    }
-
-    element = options.element = element instanceof Node ? element : false;
-    options.prop = options.prop || "height";
-    options.style = options.style || "";
-
-    if(element) {
-        index = elements.indexOf(element);
-        if(~index) {
-            datum = data[index];
         } else {
-            datum = {};
-            elements.push(element);
-            data.push(datum);
+            style[transitionProp] = "none";
+
+            startVal = getComputedStyle(el)[prop];
+            style[prop] = "auto";
+            autoVal = getComputedStyle(el)[prop];
+
+            // Interrupt
+            if(startVal === val || val === "auto" && startVal === autoVal) {
+                return;
+            }
+
+            data.auto = autoVal;
+            el.setAttribute(isTransition, 1);
+
+            // Transition
+            style[prop] = startVal;
+            el.offsetWidth;
+            style[transitionProp] = options.style;
         }
 
-        process(options, datum);
+        style[prop] = val === "auto" ? data.auto : val;
+
+        data.l = function (e) {
+            if(e.propertyName === prop) {
+                el.removeAttribute(isTransition);
+                el.removeEventListener(transitionEnd, data.l);
+                if(val === "auto") {
+                    /* avoid transition flashes in Safari */
+                    style[transitionProp] = "none";
+                    style[prop] = val;
+                }
+            }
+        };
+
+        el.addEventListener(transitionEnd, data.l);
     }
-}
 
-transition.prop = transitionProp;
-transition.end = transitionEnd;
+    /**
+    @param options {Object}
+    @param options.element {string | element} - The DOM element or selector to transition
+    @param options.val {string} - The value you want to transition to
+    @param [options.prop] {string} - The CSS property to transition, defaults to `"height"`
+    @param [options.style] {string} - The desired value for the `transition` CSS property (e.g. `"height 1s"`). If specified, this value is added inline and will override your CSS. Leave this value blank if you already have it defined in your stylesheet. 
+    @alias module:transition-to-from-auto
+    */
+    function transition(options){
+        var element = options.element;
+        var datum;
+        var index;
 
-},{}]},{},[1])(1)
-});
+        if(typeof element === "string") {
+            element = document.querySelector(element);
+        }
+
+        element = options.element = element instanceof Node ? element : false;
+        options.prop = options.prop || "height";
+        options.style = options.style || "";
+
+        if(element) {
+            index = elements.indexOf(element);
+            if(~index) {
+                datum = data[index];
+            } else {
+                datum = {};
+                elements.push(element);
+                data.push(datum);
+            }
+
+            process(options, datum);
+        }
+    }
+
+    /**
+    The name of the vendor-specific transition CSS property
+    @type {string} 
+    @example
+    el.style[transition.prop + 'Duration'] = '1s';
+    */
+    transition.prop = transitionProp;
+
+    /**
+    * The name of the [transition end event](https://developer.mozilla.org/en-US/docs/Web/Events/transitionend) in the current browser (typically `"transitionend"` or `"webkitTransitionEnd"`)
+    * @type {string} 
+    * @example
+    * el.addEventListener(transition.end, function(){
+    *     // the transition ended..
+    * });
+    */
+    transition.end = transitionEnd;
+
+
+    if (typeof module === "object" && module.exports){
+        module.exports = transition;
+    } else if (typeof define === "function" && define.amd){
+        define(function(){
+            return transition;
+        });
+    } else {
+        window.transition = transition;
+    }
+})(window, document);
